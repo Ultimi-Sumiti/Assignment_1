99747b6de4a56a7c38a9a92521bffe5dea09ca7e
luca-d <lucapiai98@proton.me>
2025-11-21 14:26:23 +0100

    Added some comments to nav2 action client

--- ws_24_assignments/src/nav_basics/src/nav2_action_client.cpp
+++ ws_24_assignments/src/nav_basics/src/nav2_action_client.cpp
@@ -2,96 +2,88 @@
 #include "rclcpp_action/rclcpp_action.hpp" 
 #include "nav2_msgs/action/navigate_to_pose.hpp" // Action interface with nav2 library
 
-#include "tf2/exceptions.h"
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/transform_listener.h"
-#include "tf2_msgs/msg/tf_message.hpp"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include "geometry_msgs/msg/point_stamped.hpp"
-#include "tf2/convert.hpp"
 #include "std_msgs/msg/string.hpp"
 
 #include <array>
 
 using namespace std::chrono_literals;
 
+// Class used to comunicate with Nav2 Action Server.
 class Nav2ActionClient : public rclcpp::Node
 {
-public:
+ public:
     using NavigateToPoseAction = nav2_msgs::action::NavigateToPose;
     using GoalHandle = rclcpp_action::ClientGoalHandle<NavigateToPoseAction>;
 
     Nav2ActionClient(const rclcpp::NodeOptions &options)
-        : Node("nav2_action_client", options)
+        : Node("nav2_action_client", options),
+          tag1_frame_("tag36h11:1"),
+          tag10_frame_("tag36h11:10"),
+          base_frame_("base_link")
     {
         // Init transform buffer and transform listener.
         tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
         tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-        // Client to interface with the action_server bt_navigator
+        // Used to interface with the action_server bt_navigator
         action_client_ = rclcpp_action::create_client<NavigateToPoseAction>(
             this, "navigate_to_pose");
 
-        // Callback Lambda function that execute the send_goal()
-        auto timer_callback_lambda = [this]()
-        { return this->send_goal(); };
-
-        // Timer to call the send_goal function, implemented with lambda function 2Hz
-        // NOTE : in the implementation below we will see that we call the function just 1 time in reality, so the freq is not so much improtant
+        // Used to execute send_goal() function.
+        auto send_goal_lambda = [this]() { return this->send_goal(); };
         this->timer_ = this->create_wall_timer(
             std::chrono::milliseconds(500),
-            timer_callback_lambda);
+            send_goal_lambda);
 
-        start_percept_publisher_ = this->create_publisher<std_msgs::msg::String>(
-            "/start_perception",
-            10
-        );
+        // Create the publisher that will start the detection process.
+        percept_publisher_ = this->create_publisher<std_msgs::msg::String>(
+            "/start_perception", 10);
     }
 
+    // Function that creates the action goal.
     void send_goal()
     {
-        using namespace std::placeholders;
-
-        // Wait to switch online the fibonacci Server otherwise after a time limit switch off the node
+        // Wait action server.
         if (!this->action_client_->wait_for_action_server())
         {
-            RCLCPP_ERROR(
-                this->get_logger(),
-                "Action server not available after waiting"
-            );
-            //rclcpp::shutdown();
+            RCLCPP_ERROR(this->get_logger(),
+                "Action server not available after waiting");
             return;
         }
 
-        // Define the goal position.
+        // Define goal position.
         set_goal_position();
+
+        // Manage the case in which /tf is not available.
         if (goal_position_[0] == 0 && goal_position_[1] == 0)
             return;
 
-        // Stop timer, just one goal is send.
+        // Stop timer, just one goal is sent.
         this->timer_->cancel();
 
-        // Goal service request
-        RCLCPP_INFO(this->get_logger(), "Sending goal... ");
+        // Create the goal request.
         auto goal_msg = nav2_msgs::action::NavigateToPose::Goal();
         goal_msg.pose.header.frame_id = "map";
         goal_msg.pose.header.stamp = this->now();
+
+        // Position.
         goal_msg.pose.pose.position.x = goal_position_[0];
         goal_msg.pose.pose.position.y = goal_position_[1];
         goal_msg.pose.pose.position.z = 0.0;
 
+        // Orientation.
         goal_msg.pose.pose.orientation.x = 0.0;
         goal_msg.pose.pose.orientation.y = 0.0;
         goal_msg.pose.pose.orientation.z = 0.0;
         goal_msg.pose.pose.orientation.w = 1.0;
         
-        RCLCPP_INFO(this->get_logger(), "Sending goal...");
+        // Define the goal options.
+        auto goal_options = rclcpp_action::Client<NavigateToPoseAction>::SendGoalOptions();
 
-        // Object to embed 3 callbacks:
-        auto send_goal_options = rclcpp_action::Client<NavigateToPoseAction>::SendGoalOptions();
-
-        // Goal response callback (Wait the acceptance responce of the Goal by the Server)
-        send_goal_options.goal_response_callback = 
+        // Response callback.
+        goal_options.goal_response_callback = 
             [this](const GoalHandle::SharedPtr &goal_handle) {
                 if (!goal_handle)
                     RCLCPP_ERROR(this->get_logger(),
@@ -101,65 +93,67 @@ public:
                         "Goal accepted by server, waiting for result");
             };
 
-        // Feedback callback (wait the feedback from Server)
-        send_goal_options.feedback_callback = 
+        // Feedback callback.
+        goal_options.feedback_callback = 
             [this](
-                    GoalHandle::SharedPtr,
-                    const std::shared_ptr<const NavigateToPoseAction::Feedback> feedback)
-            {
+                GoalHandle::SharedPtr,
+                const std::shared_ptr<const NavigateToPoseAction::Feedback> feedback
+            ) {
                 RCLCPP_INFO(this->get_logger(), 
-                    "Distance remaining: %.2f m",
-                    feedback->distance_remaining);
+                    "Distance remaining: %.2f m", feedback->distance_remaining);
             };
 
-        // Result callbacks (wait for the Result response by the Server)
-        send_goal_options.result_callback =
+        // Result callback.
+        goal_options.result_callback =
             [this](const GoalHandle::WrappedResult &result) {
                 switch (result.code)
                 {
-                    case rclcpp_action::ResultCode::SUCCEEDED:
-                        {
-                            std_msgs::msg::String start_msg;
-                            start_msg.data = "start";
-                            start_percept_publisher_->publish(start_msg);
+                case rclcpp_action::ResultCode::SUCCEEDED:
+                {
+                    // Notify perception node!
+                    std_msgs::msg::String start_msg;
+                    start_msg.data = "start";
+                    percept_publisher_->publish(start_msg);
 
-                            RCLCPP_INFO(this->get_logger(), "Goal has succeded");
-                            break;
-                        }
-                    case rclcpp_action::ResultCode::ABORTED:
-                        RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
-                        return;
-                    case rclcpp_action::ResultCode::CANCELED:
-                        RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
-                        return;
-                    default:
-                        RCLCPP_ERROR(this->get_logger(), "Unknown result code");
-                        return;
+                    RCLCPP_INFO(this->get_logger(), "Goal has succeded");
+                    break;
                 }
-
+                case rclcpp_action::ResultCode::ABORTED:
+                    RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+                    return;
+                case rclcpp_action::ResultCode::CANCELED:
+                    RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+                    return;
+                default:
+                    RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+                    return;
+                }
                 rclcpp::shutdown();
             };
 
-        this->action_client_->async_send_goal(goal_msg, send_goal_options);
+        RCLCPP_INFO(this->get_logger(), "Sending goal...");
+        this->action_client_->async_send_goal(goal_msg, goal_options);
     }
 
     void set_goal_position() {
-        // Define the two transformations.
-        geometry_msgs::msg::TransformStamped transform_tag0_base;
-        geometry_msgs::msg::TransformStamped transform_tag10_base;
+        // Transform: tag1 -> base link.
+        geometry_msgs::msg::TransformStamped T_tag1_base;
+        // Transform: tag10 -> base link.
+        geometry_msgs::msg::TransformStamped T_tag10_base;
 
+        // Init goal position to zero.
         goal_position_[0] = 0.0;
         goal_position_[1] = 0.0;
 
         try {
-            transform_tag0_base = this->tf_buffer_->lookupTransform(
+            T_tag1_base = this->tf_buffer_->lookupTransform(
                 base_frame_, // Target frame.
                 tag1_frame_, // Source frame.
                 tf2::TimePointZero,
                 std::chrono::seconds(5) // Wait up to 5 sec.
             );
 
-            transform_tag10_base = this->tf_buffer_->lookupTransform(
+            T_tag10_base = this->tf_buffer_->lookupTransform(
                 base_frame_,  // Target frame.
                 tag10_frame_, // Source frame.
                 tf2::TimePointZero,
@@ -171,51 +165,53 @@ public:
             return;
         }
 
-        // Point in vc.
-        geometry_msgs::msg::PointStamped pt_in_tag1;
-        pt_in_tag1.header.frame_id = base_frame_;
-        pt_in_tag1.header.stamp = transform_tag0_base.header.stamp;
+        // Save tag1 and tag10 position w.r.t. base link.
+        double tag1[2], tag10[2];
+        tag1[0] = T_tag1_base.transform.translation.x;
+        tag1[1] = T_tag1_base.transform.translation.y;
+        tag10[0] = T_tag10_base.transform.translation.x;
+        tag10[1] = T_tag10_base.transform.translation.y;
 
-        // Point in cs.
-        geometry_msgs::msg::PointStamped pt_in_tag10;
-        pt_in_tag10.header.frame_id = base_frame_;
-        pt_in_tag10.header.stamp = transform_tag10_base.header.stamp;
+        // Compute distance vector between tag1 -> tag10.
+        double dist[2];
+        dist[0] = tag10[0] - tag1[0];
+        dist[1] = tag10[1] - tag1[1];
 
-        // Tranform points.
-        geometry_msgs::msg::PointStamped v, u;
-        tf2::doTransform(pt_in_tag1, v, transform_tag0_base);
-        tf2::doTransform(pt_in_tag10, u, transform_tag10_base);
+        // Finally set the goal position.
+        goal_position_[0] = tag1[0] + dist[0] / 2;
+        goal_position_[1] = tag1[1] + dist[1] / 2;
 
-        // Compute r = v - u.
-        std::array<double, 2> r;
-        r[0] = u.point.x - v.point.x;
-        r[1] = u.point.y - v.point.y;
-
-        goal_position_[0] = v.point.x + r[0] / 2;
-        goal_position_[1] = v.point.y + r[1] / 2;
-
+        // Scale goal position.
         goal_position_[0] *= 0.92;
         goal_position_[1] *= 0.92;
 
         // Log tranformed point.
         RCLCPP_INFO(this->get_logger(),
-            "\n\tgoal position: x=%.3f y=%.3f z=0",
+            "Goal position: x=%.3f y=%.3f z=0",
             goal_position_[0], goal_position_[1]
         );
     }
 
     private:
+        // Used to create an action goal.
         rclcpp_action::Client<NavigateToPoseAction>::SharedPtr action_client_;
-        rclcpp::TimerBase::SharedPtr timer_;
-        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr start_percept_publisher_;
+        
+        // Used to notify perception node.
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr percept_publisher_;
 
+        // Timer used to call send_goal() function.
+        rclcpp::TimerBase::SharedPtr timer_;
+
+        // Used to get position of tags w.r.t. base frame.
         std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
         std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
 
-        const std::string tag1_frame_  = "tag36h11:1";
-        const std::string tag10_frame_ = "tag36h11:10";
-        const std::string base_frame_  = "base_link";
+        // Name of the frames.
+        const std::string tag1_frame_;
+        const std::string tag10_frame_;
+        const std::string base_frame_;
 
+        // The goal position sended to nav2 action server.
         std::array<double, 2> goal_position_;
 };
 
